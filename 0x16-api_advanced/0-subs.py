6d0b2791d46480dc44954e25a955ab72c652ab59
tolossamuel <tolossamuel1@gmail.com>
2024-03-05 22:19:34 +0300

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
-#!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = f'https://www.reddit.com/r/{subreddit}/about/.json'
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+        return response.json().get('data', {}).get('subscribers', 0)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
