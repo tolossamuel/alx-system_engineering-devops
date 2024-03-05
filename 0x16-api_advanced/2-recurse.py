7e830c07c10d7a10c8ba9445f1df23eccf20cbb1
tolossamuel <tolossamuel1@gmail.com>
2024-03-05 22:13:02 +0300

    updated

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,9 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
+def recurse(subreddit, hot_list=[]):
+    after = ""
+    count = 0
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
